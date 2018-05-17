2300cc8e7267a71e4cb29da1118091caeeed5230
Florian Guyet <florian.guyet@softbankrobotics.com>
2018-05-17 17:28:11 +0200

    Fix code issues + change the tutorial (no more infinite counting => Pepper talks when the button is clicked).

--- app/src/main/java/com/softbankrobotics/qisdktutorials/ui/tutorials/enforcetabletreachability/EnforceTabletReachabilityTutorialActivity.java
+++ app/src/main/java/com/softbankrobotics/qisdktutorials/ui/tutorials/enforcetabletreachability/EnforceTabletReachabilityTutorialActivity.java
@@ -47,14 +47,38 @@ public class EnforceTabletReachabilityTutorialActivity extends TutorialActivity 
             // Build EnforceTabletReachability action
             EnforceTabletReachability enforceTabletReachability = actuation.makeEnforceTabletReachability(qiContext.getRobotContext());
 
-            // Update button text when the action starts
-            enforceTabletReachability.setOnStartedListener(new EnforceTabletReachability.OnStartedListener() {
+            enforceTabletReachability.addOnStartedListener(new EnforceTabletReachability.OnStartedListener() {
                    @Override
                    public void onStarted() {
-                       setButtonText("Stop action");
+                       // Display log
+                       String infoLog = "The EnforceTabletReachability action has started.";
+                       displayLine(infoLog, ConversationItemType.INFO_LOG);
                    }
                }
             );
+
+            enforceTabletReachability.addOnPositionReachedListener(new EnforceTabletReachability.OnPositionReachedListener() {
+                @Override
+                public void onPositionReached() {
+                    // Display log
+                    String infoLog = "The tablet now is in position.";
+                    displayLine(infoLog, ConversationItemType.INFO_LOG);
+                    Log.i(TAG, infoLog);
+
+                    // Update button text
+                    setButtonText(getResources().getString(R.string.cancel_action));
+
+                    // Say action
+                    String textToSay = "My movements are now limited. Cancel the action to see the difference.";
+                    displayLine(textToSay, ConversationItemType.ROBOT_OUTPUT);
+
+                    Say say = SayBuilder.with(qiContext)
+                            .withText(textToSay)
+                            .build();
+
+                    say.run();
+                }
+            });
 
             // Run the action asynchronously
             enforceTabletReachabilityFuture = enforceTabletReachability.async().run();
@@ -62,11 +86,30 @@ public class EnforceTabletReachabilityTutorialActivity extends TutorialActivity 
             // Update button text when the action fishishes and log eventual errors
             enforceTabletReachabilityFuture.thenConsume(new Consumer<Future<Void>>() {
                 @Override
-                public void consume(Future<Void> voidFuture) throws Throwable {
-                    setButtonText("Enforce Tablet Reachability");
-                    if (voidFuture.hasError()) {
-                        Log.e(TAG, voidFuture.getErrorMessage());
+                public void consume(Future<Void> future) throws Throwable {
+                    // Display eventual errors
+                    if (future.hasError()) {
+                        String message = "The EnforceTabletReachability action finished with error.";
+                        Log.e(TAG, message, future.getError());
+                        displayLine(message, ConversationItemType.ERROR_LOG);
+                    } else {
+                        String message = "The EnforceTabletReachability action has finished.";
+                        Log.i(TAG, message);
+                        displayLine(message, ConversationItemType.INFO_LOG);
                     }
+
+                    // Update button text
+                    setButtonText(getResources().getString(R.string.enforce_tablet_reachability));
+
+                    // Say text when the action is cancelled
+                    String textToSay = "My movements are back to normal. Run the action again to see the difference.";
+                    displayLine(textToSay, ConversationItemType.ROBOT_OUTPUT);
+
+                    Say say = SayBuilder.with(qiContext)
+                            .withText(textToSay)
+                            .build();
+
+                    say.run();
                 }
             });
         }
@@ -119,8 +162,8 @@ public class EnforceTabletReachabilityTutorialActivity extends TutorialActivity 
     public void onRobotFocusGained(QiContext qiContext) {
         this.qiContext = qiContext;
 
-        // Introduction Say
-        String textToSay = "I can enforce my tablet reachability by limiting my movements. Try it out while I count!";
+        // Build introduction Say
+        String textToSay = "I can enforce my tablet reachability by limiting my movements. Try it out!";
         displayLine(textToSay, ConversationItemType.ROBOT_OUTPUT);
 
         Say say = SayBuilder.with(qiContext)
@@ -128,24 +171,6 @@ public class EnforceTabletReachabilityTutorialActivity extends TutorialActivity 
                 .build();
 
         say.run();
-
-        startCounting();
-    }
-
-    private void startCounting() {
-        int sayCounter = 0;
-
-        while (qiContext != null) {
-            // Say the next number
-            String numberToSay = String.valueOf(++sayCounter);
-            displayLine(numberToSay, ConversationItemType.ROBOT_OUTPUT);
-
-            Say say = SayBuilder.with(qiContext)
-                    .withText(numberToSay)
-                    .build();
-
-            say.run();
-        }
     }
 
     @Override
