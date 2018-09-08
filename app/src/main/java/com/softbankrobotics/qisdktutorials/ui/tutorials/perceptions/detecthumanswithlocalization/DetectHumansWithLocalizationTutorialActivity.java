ca0ae0083944e2f5bb090301dc9c5e9bd34aab3c
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2018-09-08 11:54:15 +1000

    Add basic content for DetectHumansWithLocalizationTutorialActivity

--- app/src/main/java/com/softbankrobotics/qisdktutorials/ui/tutorials/perceptions/detecthumanswithlocalization/DetectHumansWithLocalizationTutorialActivity.java
+++ app/src/main/java/com/softbankrobotics/qisdktutorials/ui/tutorials/perceptions/detecthumanswithlocalization/DetectHumansWithLocalizationTutorialActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.QiSDK;
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.LocalizeAndMapBuilder;
+import com.aldebaran.qi.sdk.builder.SayBuilder;
+import com.aldebaran.qi.sdk.object.actuation.LocalizeAndMap;
 import com.softbankrobotics.qisdktutorials.R;
 import com.softbankrobotics.qisdktutorials.ui.tutorials.TutorialActivity;
 
@@ -12,6 +15,8 @@ import com.softbankrobotics.qisdktutorials.ui.tutorials.TutorialActivity;
  * The activity for detecting humans with localization.
  */
 public class DetectHumansWithLocalizationTutorialActivity extends TutorialActivity implements RobotLifecycleCallbacks {
+
+    private LocalizeAndMap localizeAndMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +40,32 @@ public class DetectHumansWithLocalizationTutorialActivity extends TutorialActivi
 
     @Override
     public void onRobotFocusGained(QiContext qiContext) {
+        SayBuilder.with(qiContext)
+                .withText("I will start my localization. Please be sure to be at least at 3 meters away from me while I scan the place.")
+                .build()
+                .run();
 
+        localizeAndMap = LocalizeAndMapBuilder.with(qiContext).build();
+
+        localizeAndMap.addOnStatusChangedListener(status -> {
+            switch (status) {
+                case LOCALIZED:
+                    SayBuilder.with(qiContext)
+                            .withText("I'm now localized. Try to come to me from the side and I will turn towards you.")
+                            .build()
+                            .run();
+                    break;
+            }
+        });
+
+        localizeAndMap.async().run();
     }
 
     @Override
     public void onRobotFocusLost() {
-
+        if (localizeAndMap != null) {
+            localizeAndMap.removeAllOnStatusChangedListeners();
+        }
     }
 
     @Override
