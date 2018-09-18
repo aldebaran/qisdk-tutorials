482e7b109656d2aa7660a6950a9603bb46ed6935
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2018-09-18 17:28:12 +0200

    Add Localize action for DetectHumansWithLocalizationTutorialActivity

--- app/src/main/java/com/softbankrobotics/qisdktutorials/ui/tutorials/perceptions/detecthumanswithlocalization/DetectHumansWithLocalizationTutorialActivity.java
+++ app/src/main/java/com/softbankrobotics/qisdktutorials/ui/tutorials/perceptions/detecthumanswithlocalization/DetectHumansWithLocalizationTutorialActivity.java
@@ -8,7 +8,10 @@ import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.QiSDK;
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
 import com.aldebaran.qi.sdk.builder.LocalizeAndMapBuilder;
+import com.aldebaran.qi.sdk.builder.LocalizeBuilder;
 import com.aldebaran.qi.sdk.builder.SayBuilder;
+import com.aldebaran.qi.sdk.object.actuation.ExplorationMap;
+import com.aldebaran.qi.sdk.object.actuation.Localize;
 import com.aldebaran.qi.sdk.object.actuation.LocalizeAndMap;
 import com.aldebaran.qi.sdk.object.conversation.Say;
 import com.softbankrobotics.qisdktutorials.R;
@@ -27,6 +30,12 @@ public class DetectHumansWithLocalizationTutorialActivity extends TutorialActivi
 
     // Store the LocalizeAndMap action.
     private LocalizeAndMap localizeAndMap;
+    // Store the map.
+    private ExplorationMap explorationMap;
+    // Store the LocalizeAndMap execution.
+    private Future<Void> localizationAndMapping;
+    // Store the Localize action.
+    private Localize localize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +61,7 @@ public class DetectHumansWithLocalizationTutorialActivity extends TutorialActivi
 
     @Override
     public void onRobotFocusGained(QiContext qiContext) {
-        String textToSay = "I will start my localization. Please be sure to be at least at 3 meters away from me while I scan the place.";
+        String textToSay = "I will start mapping my environment. Please be sure to be at least at 3 meters away from me while I scan the place.";
         displayLine(textToSay, ConversationItemType.ROBOT_OUTPUT);
 
         Say say = SayBuilder.with(qiContext)
@@ -61,11 +70,79 @@ public class DetectHumansWithLocalizationTutorialActivity extends TutorialActivi
 
         say.run();
 
+        startMapping(qiContext);
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+        // Remove on status changed listeners from the LocalizeAndMap action.
+        if (localizeAndMap != null) {
+            localizeAndMap.removeAllOnStatusChangedListeners();
+        }
+        // Remove on status changed listeners from the Localize action.
+        if (localize != null) {
+            localize.removeAllOnStatusChangedListeners();
+        }
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+        // Nothing here.
+    }
+
+    private void startMapping(QiContext qiContext) {
         // Create a LocalizeAndMap action.
         localizeAndMap = LocalizeAndMapBuilder.with(qiContext).build();
 
         // Add an on status changed listener on the LocalizeAndMap action for the robot to say when he is localized.
         localizeAndMap.addOnStatusChangedListener(status -> {
+            switch (status) {
+                case LOCALIZED:
+                    explorationMap = localizeAndMap.dumpMap();
+
+                    String message = "Robot mapped his environment.";
+                    Log.i(TAG, message);
+                    displayLine(message, ConversationItemType.INFO_LOG);
+
+                    String text = "I now have a map of my environment. I will use this map to localize myself.";
+                    displayLine(text, ConversationItemType.ROBOT_OUTPUT);
+                    SayBuilder.with(qiContext)
+                            .withText(text)
+                            .build()
+                            .run();
+
+                    localizationAndMapping.requestCancellation();
+                    break;
+            }
+        });
+
+        String message = "Mapping...";
+        Log.i(TAG, message);
+        displayLine(message, ConversationItemType.INFO_LOG);
+
+        // Execute the LocalizeAndMap action asynchronously.
+        localizationAndMapping = localizeAndMap.async().run();
+
+        // Add a lambda to the action execution.
+        localizationAndMapping.thenConsume(future -> {
+            if (future.hasError()) {
+                String errorMessage = "LocalizeAndMap action finished with error.";
+                Log.e(TAG, errorMessage, future.getError());
+                displayLine(errorMessage, ConversationItemType.ERROR_LOG);
+            } else if (future.isCancelled()) {
+                startLocalizing(qiContext);
+            }
+        });
+    }
+
+    private void startLocalizing(QiContext qiContext) {
+        // Create a Localize action.
+        localize = LocalizeBuilder.with(qiContext)
+                .withMap(explorationMap)
+                .build();
+
+        // Add an on status changed listener on the Localize action for the robot to say when he is localized.
+        localize.addOnStatusChangedListener(status -> {
             switch (status) {
                 case LOCALIZED:
                     String message = "Robot is localized.";
@@ -86,30 +163,17 @@ public class DetectHumansWithLocalizationTutorialActivity extends TutorialActivi
         Log.i(TAG, message);
         displayLine(message, ConversationItemType.INFO_LOG);
 
-        // Execute the LocalizeAndMap action asynchronously.
-        Future<Void> localization = localizeAndMap.async().run();
+        // Execute the Localize action asynchronously.
+        Future<Void> localization = localize.async().run();
 
         // Add a lambda to the action execution.
         localization.thenConsume(future -> {
             if (future.hasError()) {
-                String errorMessage = "LocalizeAndMap action finished with error.";
+                String errorMessage = "Localize action finished with error.";
                 Log.e(TAG, errorMessage, future.getError());
                 displayLine(errorMessage, ConversationItemType.ERROR_LOG);
             }
         });
-    }
-
-    @Override
-    public void onRobotFocusLost() {
-        // Remove on status changed listeners from the LocalizeAndMap action.
-        if (localizeAndMap != null) {
-            localizeAndMap.removeAllOnStatusChangedListeners();
-        }
-    }
-
-    @Override
-    public void onRobotFocusRefused(String reason) {
-        // Nothing here.
     }
 
     private void displayLine(final String text, final ConversationItemType type) {
